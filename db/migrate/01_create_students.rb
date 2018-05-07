f7b1e7fa6359309ac24776374264f412e6334fa6
Veronica Kim <vero.a.kim@gmail.com>
2018-05-07 22:29:54 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  def change 
+  def change
     create_table :students do |t|
-      t.string :name 
-    end 
-  end 
+      t.string :name
+    end
+  end
 end
